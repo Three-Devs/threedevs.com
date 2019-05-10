3338d6dd9a1c4456bf6ec2da5a35fd185067a829
Luke Rebarchik <myfavoriteluke@gmail.com>
2019-05-09 21:45:09 -0500

    Clean out Hugo and false start artifacts step 1. (#24)
    * Clean out Hugo and false start artifacts step 1.
    
    * Update webpack and commit site folder.

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,5 +2,13 @@ if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
 }
 
+// Foundation.
+//import "../../../node_modules/foundation-sites/dist/css/foundation.min.css";
+//import "../../../node_modules/foundation-sites/scss/foundation.scss";
+
+// Styles.
 import "../scss/styles.scss";
+// Scripts.
+import "../js/highlight.pack.js";
+
 console.log("hello, world");
